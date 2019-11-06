2a40cc6b956d461100a0f3ba3d890fb987a03a7f
Emancus09 <send.emanuel@hotmail.com>
2019-11-06 00:16:57 -0500

    Not selecting winning moves was solved by weighting wins according to number of moves required. Added undo feature for human player. Reordered testing of moves so that moves are tested according to distance from last move.

--- XRudder.py
+++ XRudder.py
@@ -21,7 +21,12 @@ class Move:
 class HumanPlayer:
 	def makeMove(self, gameState):
 		while True:
-			args = input("To place a piece, enter the coordinates of a blank space.\nTo move a piece, enter the coordinates of your own space followed by the coordinates of the target space.\nEnter move: ").split()
+			str = input("To place a piece, enter the coordinates of a blank space.\nTo move a piece, enter the coordinates of your own space followed by the coordinates of the target space.\nEnter move: ")
+			if str == "undo":
+				gameState.undoMove()
+				gameState.undoMove()
+				return
+			args = str.split(" ")
 			try:
 				if len(args) == 2:
 					gameState.makeMove(Move(ord(args[0].lower()) - ord('a'), int(args[1]) - 1))
@@ -41,23 +46,37 @@ class HumanPlayer:
 class MiniMaxAB:
 	def makeMove(self, gameState):
 		#hard-code opening move for efficiency (makes opening 3 times faster)
-		if gameState._turn == 0:
-			gameState.makeMove(Move(7,4))
+		if gameState._turn < 2:
+			self.determineInitialKernel(gameState)
+			gameState.makeMove(Move(self.__kernelX, self.__kernelY))
 			return
 			
 		#Use minimax to find remaining moves
 		move = None
+		depth = 3
+		bound = 1000000 + depth - 1
 		if gameState._turn % 2 == 0:
-			val, move = self.getMinMove(gameState, 3, -1000000, 1000000)
+			val, move = self.getMinMove(gameState, 3, -bound, bound)
 		else:
-			val, move = self.getMaxMove(gameState, 1, -1000000, 1000000)
+			val, move = self.getMaxMove(gameState, 3, -bound, bound)
+		#Update kernel
+		self.__kernelX = move._x
+		self.__kernelY = move._y
 		gameState.makeMove(move)
+		
+	def determineInitialKernel(self, gameState):
+		#Try center
+		self.__kernelX = int(gameState._sizex / 2)
+		self.__kernelY = int(gameState._sizex / 2)
+		#Otherwise, try spot to the right
+		if (gameState._spaces[self.__kernelY][self.__kernelX] != 0):
+			self.__kernelX = self.__kernelX + 1
 
 	def getMaxMove(self, gameState, height, alpha, beta):
 		#Check if game state is final
 		winner = gameState.getWinner()
 		if winner != None:
-			return (winner * 1000000, None)
+			return (winner * (1000000 + height), None)
 			
 		#If we have reached maximum search depth, evaluate game state
 		if height == 0:
@@ -65,60 +84,63 @@ class MiniMaxAB:
 		
 		#Maximize heuristic
 		maxMove = None
-		maxValue = -1000000
+		maxValue = -1000000 - height
 		
 		piecesLeft = gameState._xPiecesLeft if gameState._turn % 2 == 0 else gameState._oPiecesLeft
 		crrtPlayer = -1 if gameState._turn % 2 == 0 else 1
-		#Iterate over spaces
-		for y in range(0,gameState._sizey):
-			for x in range(0,gameState._sizex):
-				#Try placing piece
-				if gameState._spaces[y][x] == 0 and piecesLeft > 0:
-					move = Move(x, y)
-					gameState.makeMove(move)
-					#Get minimum value of move
-					value, ignore = self.getMinMove(gameState, height - 1, alpha, beta)
-					#Select move with maximum minimum value
-					if value > maxValue:
-						maxMove = move
-						maxValue = value
-					#Try another move
-					gameState.undoMove()
+		#Iterate over spaces by spiraling around kernel
+		for w2 in range(1, max(max(gameState._sizex - self.__kernelX - 1, self.__kernelX), max(gameState._sizey - self.__kernelY - 1, self.__kernelY)) + 1):
+			#Ensure that spaces tested fall within bounds of board
+			for y in [y for y in range(self.__kernelY - w2, self.__kernelY + w2 + 1) if y >= 0 and y < gameState._sizey]:
+				#Ensure that spaces tested fall within bounds of board and border of spiral
+				for x  in [x for x in range(self.__kernelX - w2, self.__kernelX + w2 + 1) if x >= 0 and x < gameState._sizex and max(abs(x - self.__kernelX), abs(y - self.__kernelY)) == w2]:					
+					#Try placing piece
+					if gameState._spaces[y][x] == 0 and piecesLeft > 0:
+						move = Move(x, y)
+						gameState.makeMove(move)
+						#Get minimum value of move
+						value, ignore = self.getMinMove(gameState, height - 1, alpha, beta)
+						#Select move with maximum minimum value
+						if value > maxValue:
+							maxMove = move
+							maxValue = value
+						#Try another move
+						gameState.undoMove()
+						
+						#Compare against alpha-beta
+						if maxValue > alpha:
+							alpha = maxValue
+						if alpha >= beta:
+							return (maxValue, maxMove)
 					
-					#Compare against alpha-beta
-					if maxValue >= beta:
-						return (maxValue, maxMove)
-					if maxValue > alpha:
-						alpha = maxValue
-				
-				#Try shifting piece
-				elif gameState._spaces[y][x] == crrtPlayer and gameState._shiftsLeft > 0:
-					for i in [i for i in range(x - 1, x + 2) if i > -1 and i < gameState._sizex]:
-						for j in [j for j in range(y - 1, y + 2) if (j > -1 and j < gameState._sizey and not(i == x and j == y) and (gameState._spaces[j][i] == 0))]:
-							move = Move(i, j, x, y)
-							gameState.makeMove(move)
-							#Get minimum value of move
-							value, ignore = self.getMinMove(gameState, height - 1, alpha, beta)
-							#Select move with maximum minimum value
-							if value > maxValue:
-								maxMove = move
-								maxValue = value
-							#Try another move
-							gameState.undoMove()
-							
-							#Compare against alpha-beta
-							if maxValue >= beta:
-								return (maxValue, maxMove)
-							if maxValue > alpha:
-								alpha = maxValue
+					#Try shifting piece
+					elif gameState._spaces[y][x] == crrtPlayer and gameState._shiftsLeft > 0:
+						for i in [i for i in range(x - 1, x + 2) if i > -1 and i < gameState._sizex]:
+							for j in [j for j in range(y - 1, y + 2) if (j > -1 and j < gameState._sizey and not(i == x and j == y) and (gameState._spaces[j][i] == 0))]:
+								move = Move(i, j, x, y)
+								gameState.makeMove(move)
+								#Get minimum value of move
+								value, ignore = self.getMinMove(gameState, height - 1, alpha, beta)
+								#Select move with maximum minimum value
+								if value > maxValue:
+									maxMove = move
+									maxValue = value
+								#Try another move
+								gameState.undoMove()
 								
+								#Compare against alpha-beta
+								if maxValue > alpha:
+									alpha = maxValue
+								if alpha >= beta:
+									return (maxValue, maxMove)
+									
 		return (maxValue, maxMove)
 
 	def getMinMove(self, gameState, height, alpha, beta):
 		#Check if game state is final
 		winner = gameState.getWinner()
 		if winner != None:
-			return (winner * 1000000, None)
+			return (winner * (1000000 + height), None)
 			
 		#If we have reached maximum search depth, evaluate game state
 		if height == 0:
@@ -126,53 +148,56 @@ class MiniMaxAB:
 		
 		#Minimize heuristic
 		minMove = None
-		minValue = 1000000
+		minValue = 1000000 + height
 	
 		piecesLeft = gameState._xPiecesLeft if gameState._turn % 2 == 0 else gameState._oPiecesLeft
 		crrtPlayer = -1 if gameState._turn % 2 == 0 else 1
-		#Iterate over spaces
-		for y in range(0, gameState._sizey):
-			for x in range(0, gameState._sizex):
-				#Try placing piece
-				if gameState._spaces[y][x] == 0 and piecesLeft > 0:
-					move = Move(x, y)
-					gameState.makeMove(move)
-					#Get maximum value of move
-					value, ignore = self.getMaxMove(gameState, height - 1, alpha, beta)
-					#Select move with minimum maximum value
-					if value < minValue:
-						minMove = move
-						minValue = value
-					#Try another move
-					gameState.undoMove()
+		#Iterate over spaces by spiraling around kernel
+		for w2 in range(1, max(max(gameState._sizex - self.__kernelX - 1, self.__kernelX), max(gameState._sizey - self.__kernelY - 1, self.__kernelY)) + 1):
+			#Ensure that spaces tested fall within bounds of board
+			for y in [y for y in range(self.__kernelY - w2, self.__kernelY + w2 + 1) if y >= 0 and y < gameState._sizey]:
+				#Ensure that spaces tested fall within bounds of board and border of spiral
+				for x  in [x for x in range(self.__kernelX - w2, self.__kernelX + w2 + 1) if x >= 0 and x < gameState._sizex and max(abs(x - self.__kernelX), abs(y - self.__kernelY)) == w2]:
+					#Try placing piece
+					if gameState._spaces[y][x] == 0 and piecesLeft > 0:
+						move = Move(x, y)
+						gameState.makeMove(move)
+						#Get maximum value of move
+						value, ignore = self.getMaxMove(gameState, height - 1, alpha, beta)
+						#Select move with minimum maximum value
+						if value < minValue:
+							minMove = move
+							minValue = value
+						#Try another move
+						gameState.undoMove()
 
-					#Compare against alpha-beta
-					if minValue <= alpha:
-						return (minValue, move)
-					if minValue < beta:
-						beta = minValue
-				
-				#Try shifting piece
-				elif gameState._spaces[y][x] == crrtPlayer and gameState._shiftsLeft > 0:
-					for i in [i for i in range(x - 1, x + 2) if (i > -1 and i < gameState._sizex)]:
-						for j in [j for j in range(y - 1, y + 2) if (j > -1 and j < gameState._sizey and gameState._spaces[j][i] == 0)]:
-							move = Move(i, j, x, y)
-							gameState.makeMove(move)
-							#Get maximum value of move
-							value, ignore = self.getMaxMove(gameState, height - 1, alpha, beta)
-							#Select move with minimum maximum value
-							if value < minValue:
-								minMove = move
-								minValue = value
-							#Try another move
-							gameState.undoMove()
+						#Compare against alpha-beta
+						if minValue < beta:
+							beta = minValue
+						if beta <= alpha:
+							return (minValue, move)
+					
+					#Try shifting piece
+					elif gameState._spaces[y][x] == crrtPlayer and gameState._shiftsLeft > 0:
+						for i in [i for i in range(x - 1, x + 2) if (i > -1 and i < gameState._sizex)]:
+							for j in [j for j in range(y - 1, y + 2) if (j > -1 and j < gameState._sizey and gameState._spaces[j][i] == 0)]:
+								move = Move(i, j, x, y)
+								gameState.makeMove(move)
+								#Get maximum value of move
+								value, ignore = self.getMaxMove(gameState, height - 1, alpha, beta)
+								#Select move with minimum maximum value
+								if value < minValue:
+									minMove = move
+									minValue = value
+								#Try another move
+								gameState.undoMove()
 
-							#Compare against alpha-beta
-							if minValue <= alpha:
-								return (minValue, move)
-							if minValue < beta:
-								beta = minValue
-								
+								#Compare against alpha-beta
+								if minValue < beta:
+									beta = minValue
+								if beta <= alpha:
+									return (minValue, move)
+		
 		return (minValue, minMove)
 		
 	def evaluate(self, gameState):
@@ -371,7 +396,7 @@ class GameState:
 #===================================#
 
 #Initialize variables
-players = [Player('X', HumanPlayer()), Player('O', HumanPlayer())]
+players = [Player('X', HumanPlayer()), Player('O', MiniMaxAB())]
 game = GameState()
 winner = None
 
